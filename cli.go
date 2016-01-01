713608f021cce12ff3f15c0df8dc111a41d9eb31
deme0607 <hcs0035@gmail.com>
2016-01-01 18:13:57 +0900

    first commit

---
+++ cli.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+)
+
+// Exit codes are int values that represent an exit code for a particular error.
+const (
+	ExitCodeOK    int = 0
+	ExitCodeError int = 1 + iota
+)
+
+// CLI is the command line object
+type CLI struct {
+	// outStream and errStream are the stdout and stderr
+	// to write message from the CLI.
+	outStream, errStream io.Writer
+}
+
+// Run invokes the CLI with the given arguments.
+func (cli *CLI) Run(args []string) int {
+	var (
+		noEditor bool
+		params   string
+		force    bool
+		issue    string
+		base     string
+		head     string
+
+		version bool
+	)
+
+	// Define option flag parse
+	flags := flag.NewFlagSet(Name, flag.ContinueOnError)
+	flags.SetOutput(cli.errStream)
+
+	flags.BoolVar(&noEditor, "no-editor", false, "Disable opening /usr/bin/vim")
+	flags.BoolVar(&noEditor, "n", false, "Disable opening /usr/bin/vim(Short)")
+	flags.StringVar(&params, "params", "", "Parameters for template")
+	flags.StringVar(&params, "p", "", "Parameters for template(Short)")
+
+	// parameters for hub pull-request command
+	flags.BoolVar(&force, "force", false, "")
+	flags.BoolVar(&force, "f", false, "(Short)")
+	flags.StringVar(&issue, "issue", "", "")
+	flags.StringVar(&issue, "i", "", "(Short)")
+	flags.StringVar(&base, "base", "", "")
+	flags.StringVar(&base, "b", "", "(Short)")
+	flags.StringVar(&head, "head", "", "")
+	flags.StringVar(&head, "h", "", "(Short)")
+
+	flags.BoolVar(&version, "version", false, "Print version information and quit.")
+
+	// Parse commandline flag
+	if err := flags.Parse(args[1:]); err != nil {
+		return ExitCodeError
+	}
+
+	// Show version
+	if version {
+		fmt.Fprintf(cli.errStream, "%s version %s\n", Name, Version)
+		return ExitCodeOK
+	}
+
+	_ = noEditor
+
+	_ = params
+
+	_ = force
+
+	_ = issue
+
+	_ = base
+
+	_ = head
+
+	return ExitCodeOK
+}
---
+++ cli_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestRun_versionFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./ghf -version", " ")
+
+	status := cli.Run(args)
+	if status != ExitCodeOK {
+		t.Errorf("expected %d to eq %d", status, ExitCodeOK)
+	}
+
+	expected := fmt.Sprintf("ghf version %s", Version)
+	if !strings.Contains(errStream.String(), expected) {
+		t.Errorf("expected %q to eq %q", errStream.String(), expected)
+	}
+}
+
+func TestRun_noEditorFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./ghf -no-editor", " ")
+
+	status := cli.Run(args)
+	_ = status
+}
+
+func TestRun_paramsFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./ghf -params", " ")
+
+	status := cli.Run(args)
+	_ = status
+}
+
+func TestRun_fFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./ghf -f", " ")
+
+	status := cli.Run(args)
+	_ = status
+}
+
+func TestRun_iFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./ghf -i", " ")
+
+	status := cli.Run(args)
+	_ = status
+}
+
+func TestRun_bFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./ghf -b", " ")
+
+	status := cli.Run(args)
+	_ = status
+}
+
+func TestRun_hFlag(t *testing.T) {
+	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+	cli := &CLI{outStream: outStream, errStream: errStream}
+	args := strings.Split("./ghf -h", " ")
+
+	status := cli.Run(args)
+	_ = status
+}
