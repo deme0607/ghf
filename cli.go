653ccb29ded65b8ab55a739801588ca2e609a7a8
deme0607 <hcs0035@gmail.com>
2016-01-03 02:55:53 +0900

    write ghf command

--- cli.go
+++ cli.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"github.com/github/hub/cmd"
+	"github.com/github/hub/git"
+	"github.com/github/hub/github"
 	"io"
+	"io/ioutil"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
+	"text/template"
 )
 
 // Exit codes are int values that represent an exit code for a particular error.
 const (
 	ExitCodeOK    int = 0
 	ExitCodeError int = 1 + iota
+
+	ConfigFileName string = ".ghf-template"
 )
 
 // CLI is the command line object
@@ -19,15 +31,44 @@ type CLI struct {
 	outStream, errStream io.Writer
 }
 
+func (cli *CLI) getTemplate() string {
+	gitDir, err := git.Dir()
+	if err != nil {
+		fmt.Fprintf(cli.errStream, err.Error())
+		return ""
+	}
+
+	config_file := filepath.Join(gitDir, "..", ConfigFileName)
+
+	if _, err := os.Stat(config_file); err != nil {
+		current_user, _ := user.Current()
+		config_file = filepath.Join(current_user.HomeDir, ConfigFileName)
+
+		if _, err := os.Stat(config_file); err != nil {
+			fmt.Fprintf(cli.errStream, err.Error())
+			return ""
+		}
+	}
+
+	text, err := ioutil.ReadFile(config_file)
+	if err != nil {
+		fmt.Fprintf(cli.errStream, err.Error())
+		return ""
+	}
+
+	return strings.TrimRight(string(text), "\n")
+}
+
 // Run invokes the CLI with the given arguments.
 func (cli *CLI) Run(args []string) int {
 	var (
-		noEditor bool
-		params   string
-		force    bool
-		issue    string
-		base     string
-		head     string
+		noEditor      bool
+		params        string
+		template_file string
+		force         bool
+		issue         string
+		base          string
+		head          string
 
 		version bool
 	)
@@ -40,6 +81,8 @@ func (cli *CLI) Run(args []string) int {
 	flags.BoolVar(&noEditor, "n", false, "Disable opening $EDITOR(Short)")
 	flags.StringVar(&params, "params", "", "Parameters for template")
 	flags.StringVar(&params, "p", "", "Parameters for template(Short)")
+	flags.StringVar(&template_file, "template", "", "Template filename")
+	flags.StringVar(&template_file, "t", "", "Template filename(Short)")
 
 	// parameters for hub pull-request command
 	flags.BoolVar(&force, "force", false, "")
@@ -64,17 +107,65 @@ func (cli *CLI) Run(args []string) int {
 		return ExitCodeOK
 	}
 
-	_ = noEditor
+	if template_file == "" {
+		template_file = cli.getTemplate()
 
-	_ = params
+		if template_file == "" {
+			return ExitCodeError
+		}
+	}
 
-	_ = force
+	params_map := make(map[string]string)
+	if params != "" {
+		for _, p := range strings.Split(params, ",") {
+			param := strings.Split(p, ":")
+			params_map[param[0]] = param[1]
+		}
+	}
 
-	_ = issue
+	tmpl := template.Must(template.ParseFiles(template_file))
+	out := new(bytes.Buffer)
+	tmpl.Execute(out, params_map)
 
-	_ = base
+	content := out.String()
 
-	_ = head
+	if !noEditor {
+		editor, err := github.NewEditor("PULLREQ", "pull request", content)
+		if err != nil {
+			return ExitCodeError
+		}
+
+		defer editor.DeleteFile()
+
+		editor.CS = "//"
+
+		title, body, err := editor.EditTitleAndBody()
+		if err != nil {
+			return ExitCodeError
+		}
+
+		content = title + "\n\n" + body
+	}
+
+	hubCmd := cmd.New("hub pull-request")
+	hubCmd.WithArg("-m " + content)
+	if force {
+		hubCmd.WithArg("-f")
+	}
+	if issue != "" {
+		hubCmd.WithArg("-i " + issue)
+	}
+	if base != "" {
+		hubCmd.WithArg("-b " + base)
+	}
+	if head != "" {
+		hubCmd.WithArg("-h " + head)
+	}
+
+	err := hubCmd.Spawn()
+	if err != nil {
+		return ExitCodeError
+	}
 
 	return ExitCodeOK
 }
